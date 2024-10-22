ea584f9d4fd392fa4aff0d03fbc45371a53ff278
Santhosh K S <santhoshachar08@gmail.com>
2024-10-22 14:24:00 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Santhosh-KS/gohof
+
+go 1.23.2
---
+++ src/hof.go
@@ -0,0 +1,8 @@
+package hof
+
+import "fmt"
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v Welcome!", name)
+	return message
+}
